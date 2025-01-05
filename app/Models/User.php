30d30b35685c54fd378f785a0ffaa4fa120e7a78
ojoditawar <ojoditawar@gmail.com>
2025-01-05 13:18:43 +0700

    Edit User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser
